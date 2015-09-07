88292b88f23c48966d7a5cdd3d01c4d60ddcb3e4
Hawk Weisman <hi@hawkweisman.me>
2015-09-07 14:18:40 -0400

    Factor out Numeric to lib namespace (I'll need this for matrices)

--- src/vector.rs
+++ src/vector.rs
@@ -1,20 +1,4 @@
-use std::ops::{Add, Sub, Mul, Div, Rem};
-#[cfg(features = "unstable")]
-use std::num::Zero;
-
-pub trait Numeric: PartialEq + PartialOrd
-                             + Add<Self>
-                             + Sub<Self>
-                             + Mul<Self>
-                             + Div<Self>
-                             + Rem<Self>
-                             + Sized {}
-
-macro_rules! make_numeric {
-    ($($t:ty)*) => { $(impl Numeric for $t {})* };
-}
-
-make_numeric!(u8 u16 u32 u64 usize i8 i16 i32 i64 isize f32 f64);
+use super::Numeric;
 
 pub trait Vector<N>: Sized where N: Numeric {
     #[cfg(features = "unstable")]
