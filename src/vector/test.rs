467c49072105f1282bd2ab9165924f20649dcc73
Hawk Weisman <hi@hawkweisman.me>
2015-12-09 12:42:04 -0500

    Start adding QuickCheck tests

--- src/vector/test.rs
+++ src/vector/test.rs
@@ -1,16 +1,17 @@
 use super::*;
+use quickcheck::quickcheck;
 
 #[test]
 fn test_v2_addition_simple() {
     let v1 = Vector2 { x: 1, y: 1 };
-    let v2 = Vector2 { x: 2, y: 2};
+    let v2 = Vector2 { x: 2, y: 2 };
     assert_eq!(v1 + v2, Vector2 { x: 3, y: 3})
 }
 
 #[test]
 fn test_v2_subtraction_simple() {
     let v1 = Vector2 { x: 2, y: 2 };
-    let v2 = Vector2 { x: 1, y: 1};
+    let v2 = Vector2 { x: 1, y: 1 };
     assert_eq!(v1 - v2, Vector2 { x: 1, y: 1})
 }
 
@@ -20,3 +21,22 @@ fn test_v2_dot_simple() {
     let v2 = Vector2 { x: 2, y: 3 };
     assert_eq!(v1 * v2, 10)
 }
+
+macro_rules! e { ($e:expr) => { $e } }
+macro_rules! v2_arith_props {
+    ($($fun:ident, $op:tt)*) => {$(
+        #[quickcheck]
+        fn $fun( x1: isize, x2: isize
+                           , y1: isize, y2: isize) -> bool {
+            let v1 = Vector2 { x: x1, y: y1 };
+            let v2 = Vector2 { x: x2, y: y2 };
+            Vector2 { x: e!(x1 $op x2), y: e!(y1 $op y2)} == e!(v1 $op v2)
+        }
+    )*}
+}
+
+v2_arith_props!( prop_v2_addition, +
+                 prop_v2_subtraction, -
+                //  prop_v2_division, /
+                //  prop_v2_mod, %
+                );
